9ca9bebd7220b9f6766a8cafcc4835a9eaac4e3d
mjs513 <CyberMerln@aol.com>
2016-08-03 14:42:04 -0400

    Added new libs & v3 which removes telem commands

---
+++ DaguRover5AutoNavv3/BNO055Config.ino
@@ -0,0 +1,247 @@
+//============================================================================
+//    Sketch to test various technicques in robotic car design such as
+//    obstacle detection and avoidance, compass as turn guide,
+//    motor control, etc.
+//    Copyright (C) 2015  Michael J Smorto
+//    https://github.com/mjs513/Sainsmart_4WD_Obstacle_Avoid_TestBed.git
+//    FreeIMU@gmail.com
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//============================================================================
+//
+
+void BNO055_Init() {
+  
+    delay(1000);
+    telem.println("Orientation Sensor Test"); telem.println("");
+
+    /* Initialise the sensor */
+    if (!bno.begin())
+    {
+        /* There was a problem detecting the BNO055 ... check your connections */
+        telem.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        while (1);
+    }
+
+    int eeAddress = 0;
+    long bnoID;
+    bool foundCalib = false;
+
+    EEPROM.get(eeAddress, bnoID);
+
+    adafruit_bno055_offsets_t calibrationData;
+    sensor_t sensor;
+
+    /*
+    *  Look for the sensor's unique ID at the beginning oF EEPROM.
+    *  This isn't foolproof, but it's better than nothing.
+    */
+    bno.getSensor(&sensor);
+    if (bnoID != sensor.sensor_id)
+    {
+        telem.println("\nNo Calibration Data for this sensor exists in EEPROM");
+        delay(500);
+    }
+    else
+    {
+        telem.println("\nFound Calibration for this sensor in EEPROM.");
+        eeAddress += sizeof(long);
+        EEPROM.get(eeAddress, calibrationData);
+
+        displaySensorOffsets(calibrationData);
+
+        telem.println("\n\nRestoring Calibration data to the BNO055...");
+        bno.setSensorOffsets(calibrationData);
+
+        telem.println("\n\nCalibration data loaded into BNO055");
+        foundCalib = true;
+    }
+
+    delay(1000);
+
+    /* Display some basic information on this sensor */
+    displaySensorDetails();
+
+    /* Optional: Display current status */
+    displaySensorStatus();
+
+    bno.setExtCrystalUse(true);
+
+    sensors_event_t event;
+    bno.getEvent(&event);
+    if (foundCalib){
+        telem.println("Move sensor slightly to calibrate magnetometers");
+        while (!bno.isFullyCalibrated())
+        {
+            bno.getEvent(&event);
+            delay(BNO055_SAMPLERATE_DELAY_MS);
+        }
+    }
+    else
+    {
+        telem.println("Please Calibrate Sensor: ");
+        while (!bno.isFullyCalibrated())
+        {
+            bno.getEvent(&event);
+
+            telem.print("X: ");
+            telem.print(event.orientation.x, 4);
+            telem.print("\tY: ");
+            telem.print(event.orientation.y, 4);
+            telem.print("\tZ: ");
+            telem.print(event.orientation.z, 4);
+
+            /* Optional: Display calibration status */
+            displayCalStatus();
+
+            /* New line for the next sample */
+            telem.println("");
+
+            /* Wait the specified delay before requesting new data */
+            delay(BNO055_SAMPLERATE_DELAY_MS);
+        }
+    }
+
+    telem.println("\nFully calibrated!");
+    telem.println("--------------------------------");
+    telem.println("Calibration Results: ");
+    adafruit_bno055_offsets_t newCalib;
+    bno.getSensorOffsets(newCalib);
+    displaySensorOffsets(newCalib);
+
+    telem.println("\n\nStoring calibration data to EEPROM...");
+
+    eeAddress = 0;
+    bno.getSensor(&sensor);
+    bnoID = sensor.sensor_id;
+
+    EEPROM.put(eeAddress, bnoID);
+
+    eeAddress += sizeof(long);
+    EEPROM.put(eeAddress, newCalib);
+    telem.println("Data stored to EEPROM.");
+
+    telem.println("\n--------------------------------\n");
+    delay(500);
+    
+}
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bno.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Display some basic info about the sensor status
+*/
+/**************************************************************************/
+void displaySensorStatus(void)
+{
+  /* Get the system status values (mostly for debugging purposes) */
+  uint8_t system_status, self_test_results, system_error;
+  system_status = self_test_results = system_error = 0;
+  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
+
+  /* Display the results in the Serial Monitor */
+  Serial.println("");
+  Serial.print("System Status: 0x");
+  Serial.println(system_status, HEX);
+  Serial.print("Self Test:     0x");
+  Serial.println(self_test_results, HEX);
+  Serial.print("System Error:  0x");
+  Serial.println(system_error, HEX);
+  Serial.println("");
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Display sensor calibration status
+*/
+/**************************************************************************/
+void displayCalStatus(void)
+{
+  /* Get the four calibration values (0..3) */
+  /* Any sensor data reporting 0 should be ignored, */
+  /* 3 means 'fully calibrated" */
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+
+  /* The data should be ignored until the system calibration is > 0 */
+  Serial.print("\t");
+  if (!system)
+  {
+    Serial.print("! ");
+  }
+
+  /* Display the individual values */
+  Serial.print("Sys:");
+  Serial.print(system, DEC);
+  Serial.print(" G:");
+  Serial.print(gyro, DEC);
+  Serial.print(" A:");
+  Serial.print(accel, DEC);
+  Serial.print(" M:");
+  Serial.print(mag, DEC);
+}
+
+/**************************************************************************/
+/*
+    Display the raw calibration offset and radius data
+    */
+/**************************************************************************/
+void displaySensorOffsets(const adafruit_bno055_offsets_t &calibData)
+{
+    Serial.print("Accelerometer: ");
+    Serial.print(calibData.accel_offset_x); Serial.print(" ");
+    Serial.print(calibData.accel_offset_y); Serial.print(" ");
+    Serial.print(calibData.accel_offset_z); Serial.print(" ");
+
+    Serial.print("\nGyro: ");
+    Serial.print(calibData.gyro_offset_x); Serial.print(" ");
+    Serial.print(calibData.gyro_offset_y); Serial.print(" ");
+    Serial.print(calibData.gyro_offset_z); Serial.print(" ");
+
+    Serial.print("\nMag: ");
+    Serial.print(calibData.mag_offset_x); Serial.print(" ");
+    Serial.print(calibData.mag_offset_y); Serial.print(" ");
+    Serial.print(calibData.mag_offset_z); Serial.print(" ");
+
+    Serial.print("\nAccel Radius: ");
+    Serial.print(calibData.accel_radius);
+
+    Serial.print("\nMag Radius: ");
+    Serial.print(calibData.mag_radius);
+}
+
+
+
+
+
+
+
---
+++ DaguRover5AutoNavv3/Constants.h
@@ -0,0 +1,180 @@
+//============================================================================
+//    Sketch to test various technicques in robotic car design such as
+//    obstacle detection and avoidance, compass as turn guide,
+//    motor control, etc.
+//    Copyright (C) 2015  Michael J Smorto
+//    https://github.com/mjs513/Sainsmart_4WD_Obstacle_Avoid_TestBed.git
+//    FreeIMU@gmail.com
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//============================================================================
+
+#define telem Serial3
+//#define telem Serial3 // bluetooth
+
+// The serial connection to the GPS device
+#define ss Serial2
+
+//Enable 400Khz I2C bus speed
+const boolean fastmode = true;
+
+//GPS Baud Rate
+#define GPSBaud 57600
+
+//int fowardheadThreshold = 52;  	//30.48,41,51,52      headservo - obs[3]
+//int lcThreshold = 50; 	        //40.64,38,45,50      sonarlc obs[1]
+//int lcIRthreshold = 40;  //was 45
+//int sideSensorThreshold = 45;	        //50.8,38,45,41,45,36	sonarll (points to right) obs[0]
+//
+//const int obsDist = 27;
+int obsDist = 47; //was 47
+int sidedistancelimit = 37;  // was 27
+int fowardheadThreshold = 37; //was 49, 39, 29; was 27
+int lcThreshold = 36;         // was 47,27; was 26
+int lcIRthreshold = 35;  //was 45, last 47; was 27
+int sideSensorThreshold = 37; //was 42; was 27
+int max_IR_distance = 200;
+int turnCounter = 0;
+
+const int backupSensorThreshold = 17;		//17.78 - not implemented yet
+const int backup_high = 500;
+const int backup_low = 100;
+
+//Set Motor Speed
+const int speed = 50;
+const int turnSpeed = 150;
+const float lf_mtr_adj = 1.5;
+const float rr_mtr_adj = 0.97;
+const float lr_mtr_adj = 1.15;
+const float rf_mtr_adj = 1.0;
+int turn_time_mult = 2;
+int turn_time;
+
+// Ground surface type, default hard surface
+int gnd_type = 1;
+
+#define v2Amps 0.004f
+
+const int left_37 = 250;	//was 387 for 37 deg, 250 for 22deg
+const int right_37 = 295;	//was 440 for 37, 295 for 22 deg
+const int left_57 = 461;	//was 571 for 57, 461 for 45
+const int right_57 = 519;	//was 636 for 57, 519 for 45
+                                          
+#define M_PI 3.14159265359
+#define rad2deg 57.2957795
+
+//compass reads
+const int compass_avg_cnt = 20;
+const float alpha = 0.5;
+
+//sets up servo angles
+const int head_fwd = 90; 
+const int head_lft = 0;
+const int head_rt = 180;
+const int head_ldiag = 45;
+const int head_rdiag = 135;
+
+#define SONAR_NUM     4    			// Number or sensors.
+#define MAX_DISTANCE 100   			// Maximum distance (in cm) to ping. was 200
+#define PING_INTERVAL 32   			// Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
+                                          // was set to 33; was 40
+// the interval in mS 
+//#define interval 7500    //was 7500
+#define interval 100
+#define interval1 2000
+#define defaultTurnTime 1000
+#define defaultFwdTime 7000
+#define defaultRevTime 700
+#define defaultTelemTime 100
+
+//Bubble Rebound Parameters
+const float V = 21;
+const float Ki = 0.2;
+
+const int N = 25;  //was 10, 12 for 12 readings, was 12
+const int angle = 7.5;  //was 20 degrees, was 15 for 12
+
+//rcarduino constants
+#define RC_NEUTRAL_STEERING 1506  //1504 was 1506
+#define RC_NEUTRAL_THROTTLE 1504  //1500 was 1511
+
+#define RC_MAX_STEERING 1996
+#define RC_MAX_THROTTLE 1996
+
+#define RC_MIN_STEERING 1016  //1012 was 1016
+#define RC_MIN_THROTTLE 1012  //1012 was 1014
+
+#define RC_DEADBAND 30
+
+#define RC_MODE_TOGGLE 1500
+
+uint16_t unSteeringMin = RC_MIN_THROTTLE;
+uint16_t unSteeringMax = RC_MAX_STEERING;
+uint16_t unSteeringCenter = RC_NEUTRAL_STEERING;
+
+uint16_t unThrottleMin = RC_MIN_THROTTLE;
+uint16_t unThrottleMax = RC_MAX_THROTTLE;
+uint16_t unThrottleCenter = RC_NEUTRAL_THROTTLE;
+
+		  
+uint16_t ThrottleDeadBandMax = RC_NEUTRAL_THROTTLE + RC_DEADBAND;
+uint16_t ThrottleDeadBandMin = RC_NEUTRAL_THROTTLE - RC_DEADBAND;
+uint16_t SteeringDeadBandMax = RC_NEUTRAL_STEERING + RC_DEADBAND;
+uint16_t SteeringDeadBandMin = RC_NEUTRAL_STEERING - RC_DEADBAND;
+
+#define PWM_MIN 0
+#define PWM_MAX 200
+
+#define GEAR_NONE 0
+#define GEAR_IDLE 1
+#define GEAR_FULL 2
+#define GEAR_NEUTRAL 3
+
+// These bit flags are set in bUpdateFlagsShared to indicate which
+// channels have new signals
+#define THROTTLE_FLAG 1
+#define STEERING_FLAG 2
+#define RC_FLAG 3
+
+uint8_t gThrottle = 0;
+uint8_t gGear = GEAR_NONE;
+uint8_t gOldGear = GEAR_NONE;
+
+#define DIRECTION_STOP 0
+#define DIRECTION_FORWARD 1
+#define DIRECTION_REVERSE 2
+#define DIRECTION_ROTATE_RIGHT 3
+#define DIRECTION_ROTATE_LEFT 4
+#define DIRECTION_PIVOT_RIGHT 5
+#define DIRECTION_PIVOT_LEFT 6
+
+uint8_t gThrottleDirection = DIRECTION_STOP;
+uint8_t gDirection = DIRECTION_STOP;
+uint8_t gOldDirection = DIRECTION_STOP;
+
+#define IDLE_MAX 40
+
+#define MODE_RUN 1
+#define MODE_PROGRAM 0
+
+uint8_t gMode = MODE_RUN;
+uint32_t ulProgramModeExitTime = 0; 
+
+
+
+
+
+
+
